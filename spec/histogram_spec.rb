c17c69a02add4863a204769c0ddd1cc10bd23840
Joakim Karlsson <joakim@jokl.local>
2009-06-20 15:49:07 +0200

    made all tests use the TestData class

--- spec/histogram_spec.rb
+++ spec/histogram_spec.rb
@@ -1,24 +1,28 @@
 require 'lib/histogram'
 require 'lib/log_parser'
+require 'spec/test_data'
 
 describe Histogram do
 
   before(:each) do     
     @log = LogParser.new
+    @given = Given.new
   end
 
-  it "should find the correct number of times a file has changed within a day" do
-    @log.add_changedate_for_path('/the/path', Date.parse('2009-01-01'))
-    @log.add_changedate_for_path('/the/path', Date.parse('2009-01-01'))
-
+  it "should collect a file change within a day in category 0" do 
+    @given.test_data.with.logentry.at_date('2009-01-01').with.path('/the path')
+    @given.test_data.with.logentry.at_date('2009-01-01').with.path('/the path')                          
+    @log.parse(@given.test_data.to_xml) 
+    
     h = Histogram.new(@log)
  
     h.frequencies[0][1].should == 1  
   end 
   
   it "should put a file changed within 7 days correctly in the histogram" do
-    @log.add_changedate_for_path('/the/path', Date.parse('2009-01-08'))
-    @log.add_changedate_for_path('/the/path', Date.parse('2009-01-01'))
+    @given.test_data.with.logentry.at_date('2009-01-08').with.path('/the path')
+    @given.test_data.with.logentry.at_date('2009-01-01').with.path('/the path')                          
+    @log.parse(@given.test_data.to_xml) 
                
     h = Histogram.new(@log)          
                
@@ -26,26 +30,19 @@ describe Histogram do
   end 
   
   it "should put a file changed after over a year last in the histogram" do
-    @log.add_changedate_for_path('/the/path', Date.parse('2010-06-01'))
-    @log.add_changedate_for_path('/the/path', Date.parse('2009-01-01'))
+    @given.test_data.with.logentry.at_date('2010-06-01').with.path('/the path')
+    @given.test_data.with.logentry.at_date('2009-01-01').with.path('/the path')                          
+    @log.parse(@given.test_data.to_xml) 
                
     h = Histogram.new(@log)          
                
     h.frequencies[h.indexes["392-"]][1].should == 1      
   
   end  
-  
-  it "should be able to accumulate changes to several files" do
-    @log.add_changedate_for_path('/the/path', Date.parse('2010-06-01'))
-    @log.add_changedate_for_path('/the/path', Date.parse('2009-01-01'))
-               
-    h = Histogram.new(@log)          
-               
-    h.frequencies[h.indexes["392-"]][1].should == 1 
-  end
-  
+    
   it "should store a file that's only been changed once (i.e. just added to the repo) is stored correctly" do
-    @log.add_changedate_for_path('/the/path', Date.parse('2009-01-01'))
+    @given.test_data.with.logentry.at_date('2009-01-01').with.path('/the path')
+    @log.parse(@given.test_data.to_xml) 
                
     h = Histogram.new(@log)          
                
